70a15dd5b6078fb7e90ef9932ba8ef6c5db82653
Italo Fernandes <itallofernandes.89@gmail.com>
2025-10-23 23:18:32 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//efeito do botão voltar ao Topo
+
+function topo(){
+
+    window.scrollTo(
+        {
+            top:0,
+            left:0,
+            behavior: 'smooth'
+        }
+    )
+}
+
+//Validação de Login
+
+//Ativar alert no botão cadastrar
